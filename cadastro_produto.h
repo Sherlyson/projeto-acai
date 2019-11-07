79bb4ebc418cebf1819bc04853e21c184f7b1a65
jonatas junior <jonatas.junior@acad.ifma.edu.br>
2019-11-07 18:07:41 -0300

    tentativa de implementar funcoes de cadastro de produto compra e venda

--- cadastro_produto.h
+++ cadastro_produto.h
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct produto{
+typedef struct{
     char nome_produto;
     int id_produto;
     float quantidade_produto;
-};
+}produto;
 // teste cadastra produto
 // OBS: tentar tornar dinamico depois
+// Dar um jeito de o id ser sempre o anterior mais 1
 produto cadastra_produto(char nome_produto, int id_produto, float quantidade_produto ){
-    produto P;
+    produto *P,*M;
+    P = (produto *)malloc(sizeof(produto));
+    M = (produto *)malloc(3 * id_produto * sizeof(produto));
+    P->nome_produto = nome_produto;
+    P->id_produto = id_produto;
+    P->quantidade_produto = quantidade_produto;
+    for (int i = 0; i < id_produto; ++i) {
+        M[3][id_produto] = P;
+    }
+    return (M);
+}
 
-    P.nome_produto = nome_produto;
-    P.id_produto = id_produto;
-    P.quantidade_produto = quantidade_produto;
-    return (P);
+void imprime_produtos(int id_produto){
+    printf("Id do produto %d\n Nome: %c\n Quantidade: %f", )
 }
--- compra_venda.h
+++ compra_venda.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "cadastro_produto.h"
 
 float compra(int id_compra){
     printf("Digite o código id do produto comprado\n")
     scanf("%d",&id_compra)
-
+    //adicionar funçao que localiza o produto pelo id que foi armazenado no estoque e adiciona;
 }
+
+float venda(int id_produto)
